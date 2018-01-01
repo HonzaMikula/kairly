746bc0f40d88be14b60c33a53131aa26351a5832
Roman Krejcik <farin@farin.cz>
2018-01-01 21:06:24 +0100

    Update production configuration

--- kairly-server/kairly/settings_prod.py
+++ kairly-server/kairly/settings_prod.py
@@ -1,3 +1,26 @@
 from .settings import *  # NOQA
 
-# TODO add db settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'store4.rosti.cz',
+        'NAME': 'kairly_2_265101',
+        'USER': 'root',
+        'PASSWORD': 'RV3by6&#qg8@',
+        'CONN_MAX_AGE': 570,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    },
+}
+
+ALLOWED_HOSTS = [
+    'kairly-2648.rostiapp.cz',
+    'kairly.honzamikula.cz/'
+]
+
+STATICFILES_DIRS = (
+    '/srv/kairly/kairly-client/dist/static',
+)
+
+TEMPLATES[0]['DIRS'] = '/srv/kairly/kairly-client/dist'
