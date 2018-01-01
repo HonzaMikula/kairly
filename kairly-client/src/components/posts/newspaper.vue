16c65df6b98fe526f86bdc2aa5df8ce8c3fc5638
Roman Krejcik <farin@farin.cz>
2018-01-01 12:42:18 +0100

    Move continue reading logic into template

--- kairly-client/src/components/posts/newspaper.vue
+++ kairly-client/src/components/posts/newspaper.vue
@@ -2,7 +2,12 @@
   <post :post="post" v-on:readlater="readLaterMessage()">
     <timeline-post--newspaper>
       <h2>{{ post.content.title }}</h2>
-      <timeline-post--newspaper--content v-html="this.postContent"></timeline-post--newspaper--content>
+      <timeline-post--newspaper--content >
+        <div v-html="post.content.content"></div>
+        <timeline-post--continue-reading v-if="post.timeRead">
+          <a href="/post">Continue reading</a> ({{ post.timeRead }})
+        </timeline-post--continue-reading>
+      </timeline-post--newspaper--content>
     </timeline-post--newspaper>
   </post>
 </template>
@@ -13,18 +18,8 @@ import post from './post';
 export default {
   name: 'post-newspaper',
   props: ["post"],
-  data: function () {
-    return {
-      postContent: ''
-    }
-  },
   components: { post },
   created: function() {
-    var content = this.post.content.content;
-    if (this.post.timeRead) {
-      var content = this.post.content.content + '<timeline-post--continue-reading><a href="/post">Continue reading</a> ('+ this.post.timeRead +')</timeline-post--continue-reading>';
-    }
-    this.postContent = content;
   },
   methods: {
     readLaterMessage: function() {
