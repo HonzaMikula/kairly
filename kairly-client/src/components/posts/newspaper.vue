85c04680166a24ac21054a0541904b6d19bc9d1d
Jan Mikula <janm@salsitasoft.com>
2018-01-03 20:04:13 +0100

    small fix of newspaper post

--- kairly-client/src/components/posts/newspaper.vue
+++ kairly-client/src/components/posts/newspaper.vue
@@ -6,7 +6,7 @@
         <div v-html="post.content.content"></div>
         <timeline-post--continue-reading v-if="post.timeRead">
           <router-link :to="{ name: 'Post', params: { postId: post.id }}">Continue reading</router-link>
-          ({{ post.timeRead }})
+          ({{ post.timeRead }} read)
         </timeline-post--continue-reading>
       </timeline-post--newspaper--content>
     </timeline-post--newspaper>
