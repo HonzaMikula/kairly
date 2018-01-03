8aa276a0c6c5fd8d13b5f9a9fa55bea4a8e5f238
Roman Krejcik <farin@farin.cz>
2018-01-03 14:16:53 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@
 #
 # ./manage.py collectstatic --settings='kairly.settings_prod'
 
-from invoke import task
+from invoke import task, Collection
 
 # Production
 HOST = 'app@alpha-node-6.rosti.cz'
@@ -16,25 +16,67 @@ PORT = 14076
 
 
 @task
-def compile(ctx):
+def compile_js(ctx):
+    print("Deleting previous webpack build...")
     ctx.run("cd kairly-client && rm -rf dist")
+    print("Running webpack...")
     ctx.run("cd kairly-client && webpack --config build/webpack.prod.conf.js")
-    print("Webpack bundle created")
 
 
-@task(compile)
-def deploy(ctx):
+@task
+def upload_js(ctx):
     ctx.run("scp -r -P {} kairly-client/dist {}:/tmp/dist".format(PORT, HOST))
+
+
+@task(compile_js, upload_js)
+def deploy_js(ctx):
+    remote_commands = [
+        'export TERM=xterm',
+        'source /srv/.bashrc',
+        'rm -rf /srv/kairly/kairly-client/dist',
+        'mv /tmp/dist /srv/kairly/kairly-client/dist',
+        'cd /srv/app',
+        "./manage.py collectstatic --noinput --settings='kairly.settings_prod'",
+    ]
+    ctx.run("ssh -T -p {} {} '{}'".format(PORT, HOST, ' && '.join(remote_commands)))
+
+
+@task()
+def deploy_py(ctx):
     remote_commands = [
         'export TERM=xterm',
         'source /srv/.bashrc',
         'cd /srv/kairly',
         'git pull',
-        'rm -rf /srv/kairly/kairly-client/dist'
-        'mv /tmp/dist /srv/kairly/kairly-client/dist'
         'cd /srv/app',
         "./manage.py migrate --settings='kairly.settings_prod'",
         "./manage.py collectstatic --noinput --settings='kairly.settings_prod'",
-        'supervisorctl restart app'
+        'supervisorctl restart app',
     ]
     ctx.run("ssh -T -p {} {} '{}'".format(PORT, HOST, ' && '.join(remote_commands)))
+
+
+@task(compile_js, upload_js)
+def deploy_all(ctx):
+    remote_commands = [
+        'export TERM=xterm',
+        'source /srv/.bashrc',
+        'cd /srv/kairly',
+        'git pull',
+        'rm -rf /srv/kairly/kairly-client/dist',
+        'mv /tmp/dist /srv/kairly/kairly-client/dist',
+        'cd /srv/app',
+        "./manage.py migrate --settings='kairly.settings_prod'",
+        "./manage.py collectstatic --noinput --settings='kairly.settings_prod'",
+        'supervisorctl restart app',
+    ]
+    ctx.run("ssh -T -p {} {} '{}'".format(PORT, HOST, ' && '.join(remote_commands)))
+
+
+deploy_ns = Collection('deploy')
+deploy_ns.add_task(deploy_all, 'all', default=True)
+deploy_ns.add_task(deploy_js, 'js')
+deploy_ns.add_task(deploy_py, 'py')
+
+ns = Collection()
+ns.add_collection(deploy_ns)
