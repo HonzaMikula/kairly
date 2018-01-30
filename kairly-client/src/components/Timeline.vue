7f4a29efe315653648da2e82e7ff87cac11909ad
Jan Mikula <janm@salsitasoft.com>
2018-01-30 19:08:17 +0100

    fix small issues + prototype of welcome to kairly on timeline

--- kairly-client/src/components/Timeline.vue
+++ kairly-client/src/components/Timeline.vue
@@ -3,6 +3,44 @@
     infinite-scroll-disabled="loading"
     infinite-scroll-distance="100"
   >
+    <timeline-welcome>
+      <h1>Welcome to Kairly</h1>
+      <p>Start with selecting editions you want to subscribe.</p>
+
+      <timeline-welcome--my-editions>
+        <my-editions--item v-for="edition in [1,2,3]" :key="edition.id">
+          <picture>
+            <img src="http://kairly.com/media/editions/volby_omSw6dr.jpg" alt="" />
+          </picture>
+
+          <h2>Prezidentské volby</h2>
+
+          <p>Nejdůležitější informace z probíhajících prezidentských voleb.</p>
+
+          <my-editions--item--author>
+            <img src="https://pbs.twimg.com/profile_images/522497269447147520/uGF7lbPY_400x400.jpeg" alt=""/>
+            Jan Mikula
+          </my-editions--item--author>
+
+          <my-editions--item--subscribe>
+            <button>Subscribe</button>
+            <p>
+              10 CZK per month
+              •
+              {{ 123 }} subscribers
+              •
+              #{{ 12 }}
+            </p>
+          </my-editions--item--subscribe>
+
+        </my-editions--item> 
+      </timeline-welcome--my-editions>
+
+      <timeline-welcome--more-editions>
+        <router-link to='/my-editions'>View more editions</router-link>
+      </timeline-welcome--more-editions>
+    </timeline-welcome>
+
     <div v-for="issue in issues" :key="issue.id">
       <timeline-edition>
         <h1>{{issue.title}}</h1>
--- kairly-client/src/components/posts/newspaper.vue
+++ kairly-client/src/components/posts/newspaper.vue
@@ -4,7 +4,7 @@
       <h2>{{ post.content.title }}</h2>
       <timeline-post--newspaper--content>
         <div v-html="post.content.content"></div>
-        <timeline-post--continue-reading v-if="post.timeRead">
+        <timeline-post--continue-reading v-if="post.timeRead && post.timeRead !='0 min'">
           <router-link :to="{ name: 'Post', params: { postId: post.id }, hash: '#continue'}">Continue reading</router-link>
           ({{ post.timeRead }} read)
         </timeline-post--continue-reading>
