5cac934513bcde4d71b06ba48ef3fb6646cb6dec
Jan Mikula <janm@salsitasoft.com>
2018-02-28 09:34:05 +0100

    Author detail improvements

--- kairly-client/src/components/AuthorDetail.vue
+++ kairly-client/src/components/AuthorDetail.vue
@@ -1,19 +1,30 @@
 <template>
   <author-detail-view>
     <author-detail--header>
-      <h1>Jan Mikula</h1>
-      <p></p>
       <picture>
-        <img src="" alt="Jan Mikula"/>
+        <img src="https://pbs.twimg.com/profile_images/522497269447147520/uGF7lbPY_400x400.jpeg" alt="Jan Mikula"/>
       </picture>
+      <h1>Jan Mikula</h1>
+      <p>Mé názory nevyjadřují názory Kairly ani CzechTV.</p>
     </author-detail--header>
 
+    <author-detail--subscribe>
+      <button>Subscribe Editions</button>
+
+      <button>Subscribe Posts</button>
+    </author-detail--subscribe>
+
     <author-detail--editions>
-      here will be edition boxes
+      <h2>Jan Mikula's Editions</h2>
+     
+      <img src="../assets/kairly-temp-editions.png" />
+
     </author-detail--editions>
 
     <author-detail--posts>
-      here will be posts display same way as on timeline
+      <h2>Jan Mikula's Posts</h2>
+
+      <img src="../assets/kairly-temp-posts.png" />
     </author-detail--posts>
 
   </author-detail-view>
