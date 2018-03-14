3adf20f93ce56cc13a8de61a0f058d3ae24d1896
Jan Mikula <janm@salsitasoft.com>
2018-03-14 09:29:22 +0100

    fix issue with tweet displaying wrong + remove unused styles

--- kairly-client/src/components/posts/post.vue
+++ kairly-client/src/components/posts/post.vue
@@ -1,5 +1,5 @@
 <template>
-  <timeline-post role="article">
+  <timeline-post role="article" :class="post.type">
       <timeline-post--header v-on:mouseleave="closeAuthorWidget()">
         <img :src="post.author.picture" :alt="post.author.name" />
 
