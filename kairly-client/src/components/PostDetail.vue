eff86a4713688e33603719a765169ed76eef8bca
Jan Mikula <janm@salsitasoft.com>
2018-03-24 12:26:52 +0100

    post detail improvements

--- kairly-client/src/components/PostDetail.vue
+++ kairly-client/src/components/PostDetail.vue
@@ -5,14 +5,18 @@
       v-on:click="$router.go(-1)">
     </post-detail--back-button>
 
+    <post-detail--read-later
+      v-tooltip.right="'Read Later'">
+    </post-detail--read-later>
+
     <div v-if="post">
       <post-detail--header>
-        <img :src="post.author.picture" :alt="post.author.name"/>
-        <author-name>{{post.author.name}}</author-name>
-        <author-follow><router-link :to="post.author.url">Author detail</router-link></author-follow>
-        <author-description>{{post.author.bio}}</author-description>
+        <router-link :to="post.author.url">
+          <img :src="post.author.picture" :alt="post.author.name"/>
+          {{post.author.name}}<span v-if="post.author.medium">, {{post.author.medium}}</span>
+        </router-link>
 
-        <button-read-later></button-read-later>
+        <button-icon class="read-later" v-tooltip.top="'Read Later'"></button-icon>
       </post-detail--header>
 
       <post-detail--title>
@@ -28,10 +32,26 @@
       <post-detail--content v-html="post.content.content"></post-detail--content>
 
       <post-detail--footer>
-        <button-icon class="favorite" v-tooltip.top="'Favorite'"></button-icon>
-        <button-icon class="share" v-tooltip.top="'Share'"></button-icon>
-        <button-icon class="edition" v-tooltip.top="'Consider for Edition'"></button-icon>
+        <button-icon class="recommend">Recommend</button-icon>
+        <button-icon class="share">Share</button-icon>
+        <button-icon class="consider-for-edition">Consider for Edition</button-icon>
       </post-detail--footer>
+
+      <post-detail--author>
+        <picture>
+          <router-link :to="post.author.url">
+            <img :src="post.author.picture" :alt="post.author.name"/>
+          </router-link>
+        </picture>
+        
+        <h3>
+          <router-link :to="post.author.url">
+            {{post.author.name}}<span v-if="post.author.medium">, {{post.author.medium}}</span>
+          </router-link>
+        </h3>
+
+        <p>{{post.author.bio}}</p>
+      </post-detail--author>
 
     </div>
   </post-detail>
