e4d6ae55d9da23dfa7f5f813b12d0f95d8879349
Jan Mikula <janm@salsitasoft.com>
2018-03-26 09:32:40 +0200

    #30 fix mobile issues

--- kairly-client/src/components/issues/author.vue
+++ kairly-client/src/components/issues/author.vue
@@ -23,13 +23,30 @@
     />
 
     <post-issue>
-      <h2><a href="/editions/janmikula/malostranskenoviny">Malostranské noviny #14</a></h2>
-      <picture>
-        <a href="/editions/janmikula/malostranskenoviny">
-          <img src="http://kairly.com/media/editions/malostranskenoviny.jpg"/>
+      <timeline-post--header>
+        <a href="">
+          <img src="https://pbs.twimg.com/profile_images/522497269447147520/uGF7lbPY_400x400.jpeg"/>
         </a>
-      </picture>
-      <p>Přehled toho nejzajímavějšího, co se událo v české politice.</p>
+
+        <timeline-post--header--author>
+          <a href="">Jan Mikula</a>
+        </timeline-post--header--author>
+
+        <timeline-post--header--info>
+          Last Friday at 10:41 PM
+        </timeline-post--header--info>
+
+      </timeline-post--header>
+
+      <div>
+        <h2><a href="/editions/janmikula/malostranskenoviny">Malostranské noviny #14</a></h2>
+        <picture>
+          <a href="/editions/janmikula/malostranskenoviny">
+            <img src="http://kairly.com/media/editions/malostranskenoviny.jpg"/>
+          </a>
+        </picture>
+        <p>Přehled toho nejzajímavějšího, co se událo v české politice.</p>
+      </div>
     </post-issue>
 
   </div>
