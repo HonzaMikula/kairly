9a0e63d19c14e8b35c6200e7a997ada32819daa3
Jan Mikula <janm@salsitasoft.com>
2018-03-27 10:14:06 +0200

    Resolved #31 - if author has too many editions, display only top editions + some fixes

--- kairly-client/src/components/AuthorDetail.vue
+++ kairly-client/src/components/AuthorDetail.vue
@@ -1,32 +1,35 @@
 <template>
   <author-detail-view>
-    <div v-if="loading">...</div>
+    <loading-spinner v-if="loading"></loading-spinner>
+
     <div v-else>
       <author-detail--header>
         <picture>
           <img :src="author.picture" :alt="author.name"/>
         </picture>
         <h1>{{ author.name }}</h1>
-        <p>{{ author.medium }}</p>
+        <p>{{ author.bio }}</p>
       </author-detail--header>
 
       <author-detail--subscribe>
         <button
           v-bind:class="{ 'is-subscribed': author.isSubscribed }"
           v-on:click="subscribe($event)"
-        >{{ author.isSubscribed ? 'Unfollow Author' : 'Follow Author'}}</button>
+        >{{ author.isSubscribed ? 'Unfollow author' : 'Follow author'}}</button>
       </author-detail--subscribe>
 
       <author-detail--editions v-if="editions.length">
         <h2>{{ author.name }}'s Editions</h2>
 
-        <div style="display: flex">
+        <div>
           <MyEditionsItem
             v-for="edition in editions"
             :key="edition.id"
             v-bind:edition="edition"
           />
         </div>
+
+        <button v-if="allEditions.length > 3" v-on:click="toggleEditions()">{{ !isAllEditionsOpened ? 'Show all editions' : 'Hide editions' }}</button>
 
       </author-detail--editions>
 
@@ -36,7 +39,7 @@
         <PostWrapper
           v-for="post in posts"
           :post="post"
-          :isSubscribed="false"
+          :isSubscribed="author.isSubscribed"
           :key="post.id"
         />
       </author-detail--posts>
@@ -64,6 +67,9 @@ export default {
     return {
       'loading': true,
       'author': null,
+      'isAllEditionsOpened': false,
+      'ellEditions': [],
+      'topEditions': [],
       'editions': [],
       'posts': []
     }
@@ -75,14 +81,25 @@ export default {
       api.postAuthorSubsription(this.author, value).then(author => this.author)
       this.author.isSubscribed = value
       ev.target.blur()
+    },
+
+    toggleEditions() {
+      if (!this.isAllEditionsOpened)
+        this.editions = this.allEditions
+      else
+        this.editions = this.topEditions 
+
+      this.isAllEditionsOpened = !this.isAllEditionsOpened  
     }
   },
 
   created() {
     api.getAuthorDetail(this.$route.params.authorId).then(resp => {
       this.author = resp.author
-      this.editions = resp.editions
+      this.allEditions = resp.editions
+      this.topEditions = resp.editions.slice(0, 3)
       this.posts = resp.posts
+      this.editions = this.topEditions
       this.loading = false
     })
   }
