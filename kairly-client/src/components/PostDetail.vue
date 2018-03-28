d934aef04effcb7994d6d625222af78311f9f8e6
Jan Mikula <janm@salsitasoft.com>
2018-03-28 17:59:18 +0200

    fix some styling issues

--- kairly-client/src/components/PostDetail.vue
+++ kairly-client/src/components/PostDetail.vue
@@ -1,58 +1,61 @@
 <template>
   <post-detail role="article">
-    <post-detail--back-button
-      v-tooltip.right="'Back to Browsing Editions'"
-      v-on:click="$router.go(-1)">
-    </post-detail--back-button>
-
-    <post-detail--read-later
-      v-tooltip.right="'Read Later'">
-    </post-detail--read-later>
+    <loading-spinner v-if="!post"></loading-spinner>
 
     <div v-if="post">
-      <post-detail--header>
-        <router-link :to="post.author.url">
-          <img :src="post.author.picture" :alt="post.author.name"/>
-          {{post.author.name}}<span v-if="post.author.medium">, {{post.author.medium}}</span>
-        </router-link>
+      <post-detail--back-button
+        v-tooltip.right="'Back to Browsing Editions'"
+        v-on:click="$router.go(-1)">
+      </post-detail--back-button>
 
-        <button-icon class="read-later" v-tooltip.top="'Read Later'"></button-icon>
-      </post-detail--header>
+      <post-detail--read-later
+        v-tooltip.right="'Read Later'">
+      </post-detail--read-later>
 
-      <post-detail--title>
-        <h1>{{post.content.title}}</h1>
-      </post-detail--title>
-
-      <post-detail--content v-html="post.content.perex"></post-detail--content>
-
-      <post-detail--continue-reading id="continue">
-        continue reading
-      </post-detail--continue-reading>
-
-      <post-detail--content v-html="post.content.content"></post-detail--content>
-
-      <post-detail--footer>
-        <button-icon class="recommend">Recommend</button-icon>
-        <button-icon class="share">Share</button-icon>
-        <button-icon class="consider-for-edition">Consider for Edition</button-icon>
-      </post-detail--footer>
-
-      <post-detail--author>
-        <picture>
+      <main>
+        <post-detail--header>
           <router-link :to="post.author.url">
             <img :src="post.author.picture" :alt="post.author.name"/>
-          </router-link>
-        </picture>
-        
-        <h3>
-          <router-link :to="post.author.url">
             {{post.author.name}}<span v-if="post.author.medium">, {{post.author.medium}}</span>
           </router-link>
-        </h3>
 
-        <p>{{post.author.bio}}</p>
-      </post-detail--author>
+          <button-icon class="read-later" v-tooltip.top="'Read Later'"></button-icon>
+        </post-detail--header>
 
+        <post-detail--title>
+          <h1>{{post.content.title}}</h1>
+        </post-detail--title>
+
+        <post-detail--content v-html="post.content.perex"></post-detail--content>
+
+        <post-detail--continue-reading id="continue">
+          continue reading
+        </post-detail--continue-reading>
+
+        <post-detail--content v-html="post.content.content"></post-detail--content>
+
+        <post-detail--footer>
+          <button-icon class="recommend">Recommend</button-icon>
+          <button-icon class="share">Share</button-icon>
+          <button-icon class="consider-for-edition">Consider for Edition</button-icon>
+        </post-detail--footer>
+
+        <post-detail--author>
+          <picture>
+            <router-link :to="post.author.url">
+              <img :src="post.author.picture" :alt="post.author.name"/>
+            </router-link>
+          </picture>
+          
+          <h3>
+            <router-link :to="post.author.url">
+              {{post.author.name}}<span v-if="post.author.medium">, {{post.author.medium}}</span>
+            </router-link>
+          </h3>
+
+          <p>{{post.author.bio}}</p>
+        </post-detail--author>
+      </main>
     </div>
   </post-detail>
 </template>
