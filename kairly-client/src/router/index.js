758da1260118e6fc1426c388d683ab0870592f31
Roman Krejcik <farin@farin.cz>
2018-06-01 09:54:20 +0200

    routes code style - use compact indent

--- kairly-client/src/router/index.js
+++ kairly-client/src/router/index.js
@@ -14,48 +14,16 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'timeline',
-      component: Timeline
-    }, {
-      path: '/post/:postId',
-      name: 'post',
-      component: PostDetail
-    }, {
-      path: '/editions/:editionId+',
-      name: 'edition',
-      component: EditionDetail
-    }, {
-      path: '/author/:authorId',
-      name: 'author',
-      component: AuthorDetail
-    }, {
-      path: '/homepage',
-      name: 'homepage',
-      component: Homepage
-    }, {
-      path: '/subscription',
-      component: MySubscription,
-      children: [
-        {
-           path: '',
-           name: 'subscription',
-           redirect: { name: 'subscription.editions' }
-        },
-        {
-          path: 'editions',
-          name: 'subscription.editions',
-          component: MyEditions
-        },
-        {
-          path: 'authors',
-          name: 'subscription.authors',
-          component: MyAuthors
-        }
-      ]
-
-    }
+    {path: '/', name: 'timeline', component: Timeline},
+    {path: '/post/:postId', name: 'post', component: PostDetail},
+    {path: '/editions/:editionId+', name: 'edition', component: EditionDetail},
+    {path: '/author/:authorId', name: 'author', component: AuthorDetail},
+    {path: '/homepage', name: 'homepage', component: Homepage},
+    {path: '/subscription', component: MySubscription, children: [
+      {path: '', name: 'subscription', redirect: { name: 'subscription.editions'}},
+      {path: 'editions', name: 'subscription.editions', component: MyEditions},
+      {path: 'authors', name: 'subscription.authors', component: MyAuthors}
+    ]}
   ],
   linkActiveClass: 'is-active',
   linkExactActiveClass: 'is-active'
