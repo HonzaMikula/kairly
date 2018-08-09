3f5b1dc94e2ba91b3dc4696ecaf7c34edb90857c
Jan Mikula <janm@salsitasoft.com>
2018-08-09 12:09:29 +0200

    allow open images from twitter

--- kairly-client/src/components/posts/TweetAttachmentPhoto.vue
+++ kairly-client/src/components/posts/TweetAttachmentPhoto.vue
@@ -1,6 +1,8 @@
 <template>
   <tweet-attachment-gallery :class="'gallery-'+ items.length">
-    <img v-for="item in items" :key="item.href" :src="`${item.src}:small`" :width="item.sizes.small.w" :height="item.sizes.small.h" />
+    <a :href="`${item.src}:large`" v-for="item in items" :key="item.id" target="_blank">
+      <img :src="`${item.src}:small`" :width="item.sizes.small.w" :height="item.sizes.small.h" />
+    </a>
   </tweet-attachment-gallery>
 </template>
 
@@ -22,24 +24,24 @@ tweet-attachment-gallery
   margin-top: $baseline / 2
 
   &.gallery-1
-    grid-template-columns: 1fr
-    grid-template-rows: 1fr
+    grid-template-columns: 100%
+    grid-template-rows: 100%
 
   &.gallery-2
-    grid-template-columns: 1fr 1fr
-    grid-template-rows: 1fr
+    grid-template-columns: 50% 50%
+    grid-template-rows: 100%
 
   &.gallery-3
-    grid-template-columns: 1fr 1fr
-    grid-template-rows: 1fr 1fr
+    grid-template-columns: 50% 50%
+    grid-template-rows: 50% 50%
 
-    img:nth-of-type(2)
+    a:nth-of-type(2)
       grid-column: 2 / span 1
       grid-row: 1 / span 2
 
   &.gallery-4
-    grid-template-columns: 1fr 1fr
-    grid-template-rows: 1fr 1fr
+    grid-template-columns: 50% 50%
+    grid-template-rows: 50% 50%
 
   img
     object-fit: cover
