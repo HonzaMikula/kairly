5dd9495e3dd853e89eccf5d0e42669ac0c33f0b1
Roman Krejcik <farin@farin.cz>
2018-08-30 22:41:37 +0200

    Fix explore link in dropdown

--- client/components/layout/Header.vue
+++ client/components/layout/Header.vue
@@ -47,7 +47,7 @@
           <li class="my-newspapers"><nuxt-link :to="{name: 'newspapers'}"><span>My Newspapers</span></nuxt-link></li>
           <li><nuxt-link :to="{name: 'user-settings'}"><span>Settings</span></nuxt-link></li>
           <li class="divider"></li>
-          <li class="explore"><nuxt-link :to="{name: 'explore'}"><span>Explore</span></nuxt-link></li>
+          <li class="explore"><nuxt-link :to="{name: 'explore-tab'}"><span>Explore</span></nuxt-link></li>
           <li><a href="" v-on:click.prevent="isTutorialOpen=true">Help</a></li>
           <li><a href="" v-on:click.prevent="logout">Logout</a></li>
         </ul>
