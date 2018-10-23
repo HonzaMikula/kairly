53029729c7833b3444e6dca93d08a1a3bc1767da
Jan Mikula <janm@salsitasoft.com>
2018-10-23 18:31:53 +0200

    more compelling Join us modal

--- client/components/modals/JoinUs.vue
+++ client/components/modals/JoinUs.vue
@@ -6,21 +6,37 @@
         <button-close tabindex="0" role="button" @click="closeModal()"></button-close>
       </header>
       <div>
-        <p>Here are ways how you can help us:</p>
-        <ul>
-          <li>
-            Join our
-            <a href="https://www.facebook.com/groups/276916213077456/">Facebook group</a>
-          </li>
-          <li>
-            Participate in
-            <a href="mailto:info@kairly.com?subject=Kairly user testing&body=Hi, I would like to do user testing for Kairly.">user testing over Skype</a>
-          </li>
-          <li>
-            Request to join
-            <a href="mailto:info@kairly.com?subject=Kairly join beta&body=Hi, I would like to join Kairly private beta.">private beta program</a>
-          </li>
-        </ul>
+        <h3>Participate in user testing</h3>
+        <p>
+          We are doing 30 - 60 minutes user testing interviews over Skype (or some other communication tool).
+          We will give you access to Kairly. You will share screen with us. We will observe how you use Kairly.
+          Together we will discuss what you like, what you don't like and how we can improve the app.
+        </p>
+
+        <p>
+          <a href="mailto:info@kairly.com?subject=Kairly user testing&body=Hi, I would like to do user testing for Kairly.">Schedule user testing interview</a>
+        </p>
+
+        <h3>Join private beta</h3>
+        <p>
+          We are inviting limited number of people to use Kairly and give us feedback about the app.
+          Based on the feedback we are improving the app every week. Although there can be many bugs in the app,
+          you can experience our ideas how to improve current state of journalism.
+        </p>
+        <p>
+          <a href="mailto:info@kairly.com?subject=Kairly join beta&body=Hi, I would like to join Kairly private beta.">Join private beta</a>
+        </p>
+
+        <h3>Join Facebook group</h3>
+        <p>
+          We have Kairly group on Facebook, where we regularly share what's new in Kairly and what we are planning for next weeks.
+          It's place where people give us feedback and propose ideas, how we can improve Kairly.
+        </p>
+
+        <p>
+          <a href="https://www.facebook.com/groups/276916213077456/">Join Facebook group</a>
+        </p>
+
       </div>
     </modal-dialog>
   </dialog-window>
@@ -45,27 +61,42 @@ export default {
 
 <style lang="sass">
 modal-dialog.join-us
+  max-width: 600px
 
   > div
     padding: $baseline
+    background: #fff
+
+  h3
+    margin-top: $baseline
+    font-weight: 600
+    font-size: $fs-1
+
+    &:first-of-type
+      margin-top: 0
 
   p
-    margin-bottom: $baseline
+    margin-bottom: $baseline / 2
 
-  li
-    list-style: disc
-    margin-left: $baseline
+    line-height: 1.42
 
-    font-size: $fs--1
-    white-space: nowrap
 
-    a
-      color: $c-base
+  a
+    display: inline-block
+    border-radius: 5px
+    padding: 0 $baseline/2
 
-      text-decoration: underline
+    background: $c-base
+    border: 1px solid $c-base
+    color: #fff
 
-      &:focus,
-      &:hover
-        text-decoration: none
+    cursor: pointer
+    font-family: $ff-sans
+    font-size: $fs-0
+    line-height: $baseline
+
+    &:focus,
+    &:hover
+      background: darken($c-base, 10%)
 
 </style>
