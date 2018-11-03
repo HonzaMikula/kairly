8ffa6edf9cb6f514816362773a9b8e3805eb695e
Roman Krejcik <farin@farin.cz>
2018-11-03 22:00:11 +0100

    Post sanitizer, allow data URIs

--- kairly-server/utils/html.py
+++ kairly-server/utils/html.py
@@ -1,18 +1,21 @@
 import bleach
 
 ALLOWED_TAGS = [
-  'a', 'abbr', 'acronym', 'b', 'br', 'blockquote', 'code', 'div', 'em',
-  'h2', 'h3', 'h4',
-  'i', 'img',
-  'li', 'ol', 'p', 'span', 'strong', 'u', 'ul'
+    'a', 'abbr', 'acronym', 'b', 'br', 'blockquote', 'code', 'div', 'em',
+    'h2', 'h3', 'h4',
+    'i', 'img',
+    'li', 'ol', 'p', 'span', 'strong', 'u', 'ul'
 ]
 ALLOWED_ATTRIBUTES = {
-  'a': ['href', 'title'],
-  'abbr': ['title'],
-  'acronym': ['title'],
-  'img': ['src', 'alt', 'title', 'width', 'height']
+    'a': ['href', 'title'],
+    'abbr': ['title'],
+    'acronym': ['title'],
+    'img': ['src', 'alt', 'title', 'width', 'height']
 }
+# allow data URIs
+ALLOWED_PROTOCOLS = ['http', 'https', 'data']
 
 
 def sanitize(text):
-    return bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)
+    return bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES,
+                        protocols=ALLOWED_PROTOCOLS)
