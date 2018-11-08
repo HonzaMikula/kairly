c55805a635c09523a6ff15d3e1ef91f0e5d21422
Roman Krejcik <farin@farin.cz>
2018-11-08 16:43:30 +0100

    Separate cron instance

--- kairly-server/cron/importrss.sh
+++ kairly-server/cron/importrss.sh
@@ -3,4 +3,4 @@
 . /srv/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=kairly.settings_prod
 export PYTHONIOENCODING=utf-8
-/srv/app/manage.py importrss
+/srv/kairly/kairly-server/manage.py importrss
