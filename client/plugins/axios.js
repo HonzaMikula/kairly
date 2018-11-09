f12ee363f0837f760bd3b54259881d1f9e36e045
Roman Krejcik <farin@farin.cz>
2018-11-09 22:41:18 +0100

    Add retrying to client -> api requests
    Fixes #189.

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,4 +1,10 @@
+import axiosRetry from 'axios-retry';
+
 export default function({ $axios, redirect }) {
+  axiosRetry($axios, {
+    retryDelay: axiosRetry.exponentialDelay
+  })
+
   $axios.onError(err => {
     const code = parseInt(err.response && err.response.status)
     if (code === 401) {
