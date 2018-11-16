21093197c7a6e258b5171adf1ae7a17e7c9a7c85
Roman Krejcik <farin@farin.cz>
2018-11-16 12:12:12 +0100

    Make newspaper image optional in admin form

--- kairly-server/articles/models.py
+++ kairly-server/articles/models.py
@@ -137,7 +137,7 @@ class Newspaper(models.Model, PeriodMixin):
     title = models.CharField(max_length=160)
     slug = models.SlugField(_('Slug'))
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='editions', null=True)
+    image = models.ImageField(upload_to='editions', null=True, blank=True)
     editor = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, null=True)
 
     period = models.CharField(max_length=32, choices=PeriodMixin.PERIOD_CHOICES, default=PeriodMixin.DAILY)
