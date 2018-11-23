fc6f1d72a4730352de6cf378bf759e3e936e4631
Roman Krejcik <farin@farin.cz>
2018-11-23 18:13:57 +0100

    Fix 404 pages
    Broken by 401 interceptor which catches all errors by mistake

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -12,6 +12,8 @@ export default function({ app, $axios, redirect }) {
       // delete all local tokens
       await app.$auth.logout()
       redirect('/')
+    } else {
+      throw err
     }
   })
 
