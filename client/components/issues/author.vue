5c601c7bd4ddb18c53cb2915ed1e24fffdf8968a
Roman Krejcik <farin@farin.cz>
2018-12-10 10:47:13 +0100

    Translate author newspaper titles

--- client/components/issues/author.vue
+++ client/components/issues/author.vue
@@ -9,7 +9,7 @@
       </h1>
 
       <p>
-        {{ issue.title }}
+        {{ localizedTitle() }}
         •
         {{ issue.time | moment('calendar') }}
       </p>
@@ -41,5 +41,17 @@ export default {
   components: {
     PostIssue
   },
+
+  methods: {
+    localizedTitle() {
+      // each constant must be wrapped to $t
+      const { title } = this.issue
+      if (title === '6× per day') return this.$t('6× per day')
+      if (title === '3× per day') return this.$t('3× per day')
+      if (title === 'Daily summary') return this.$t('Daily summary')
+      if (title === 'Weekly summary') return this.$t('Weekly summary')
+      return title // should never happen
+    }
+  }
 }
 </script>
