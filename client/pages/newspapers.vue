665450dc6a46611981aad9cf0398d8f8c92697b3
Jan Mikula <janm@salsitasoft.com>
2018-12-28 09:54:34 +0100

    reverse order of posts in Backlog
    closes #237

--- client/pages/newspapers.vue
+++ client/pages/newspapers.vue
@@ -228,7 +228,8 @@ export default {
 
       this.isBacklogLoaded = false
       const { backlog, publish } = await this.$axios.$get(`/newspapers/${fullName}/backlog`)
-      this.postsBacklog = backlog
+      this.postsBacklog = backlog.reverse()
+      console.log(backlog)
       this.postsPublished = publish
       this.isBacklogLoaded = true
     },
