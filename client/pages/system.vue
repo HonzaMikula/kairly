667542e874b3409d361b6480e9dee6877f990e2c
Jan Mikula <janm@salsitasoft.com>
2019-02-13 08:48:46 +0100

    basic styling for system transactions

--- client/pages/system.vue
+++ client/pages/system.vue
@@ -5,23 +5,44 @@
         <h1>System Reports</h1>
       </header>
 
-      Platform credits balance {{ credits }}
+      <section class="system-transactions--balance">
+        Platform credits balance <strong>{{ credits }}</strong> Kč
+      </section>
 
       <h2>Platform Transactions</h2>
 
-      <table class="transactions--table">
+      <table class="system-transactions--table">
         <thead>
           <tr>
             <th>Datetime</th>
             <th>Credits</th>
-            <th>From ... To</th>
+            <th>From</th>
+            <th>To</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="t in transactions" :key="t.created">
             <td>{{ t.created | moment('calendar') }}</td>
-            <td>{{ t.credits > 0 ? '+' : ''}}{{ t.credits }}</td>
-            <td>{{ t.source }} ... {{ t.target }}</td>
+            <td>{{ t.credits > 0 ? '+' : ''}}{{ t.credits }} Kč</td>
+
+            <td v-if="t.source.newspaper">
+              <!-- Monthly reward for newspaper editor -->
+              <img :src="t.source.newspaper.picture" :alt="t.source.newspaper.name" />
+              {{ t.source.newspaper}}
+            </td>
+            <td v-else-if="t.source.author">
+              <!-- Monthly reward for author -->
+              <img :src="t.source.author.picture" :alt="t.source.author.name" />
+              {{ t.source.author.name }}
+            </td>
+            <td v-else>System</td>
+
+            <td v-if="t.target.user">
+              <!-- Monthly reward for author -->
+              <img v-if="t.target.user.picture" :src="t.target.user.picture" :alt="t.target.user.name" />
+              {{ t.target.user.name }}
+            </td>
+            <td v-else>System</td>
           </tr>
         </tbody>
       </table>
@@ -67,24 +88,17 @@ export default {
 </script>
 
 <style lang="sass">
-.transactions
-  margin: $baseline auto
-  max-width: 900px
+.system-transactions--balance
+  margin-bottom: $baseline
 
-  header
-    display: flex
-    margin-bottom: $baseline / 2
-
-    //- heading
-    > h1
-      flex: 1
-
-      font-size: $fs-3
-      font-weight: 600
+  font-size: $fs-1
 
 
-.transactions--table
+.system-transactions--table
   width: 100%
+
+  a
+    color: #000
 
   thead th
     padding: $baseline/4
@@ -96,8 +110,21 @@ export default {
 
   tbody th,
   tbody td
-    padding: $baseline/4
+    padding: $baseline / 4
 
     border-bottom: 1px solid #eee
+
+    line-height: $baseline * 1.25
+
+    img
+      float: left
+      height: $baseline * 1.25
+      margin-right: $baseline / 4
+      width: $baseline * 1.25
+
+      object-fit: cover
+
+  td.author img
+    border-radius: 100%
 
 </style>
