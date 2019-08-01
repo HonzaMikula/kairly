f683078816565120fa4d87d15bf390b7a9fb0a09
Jan Mikula <janm@salsitasoft.com>
2019-08-01 10:06:31 +0200

    styling for reset password and change password after reset
    closes #386

--- client/pages/reset-password/_token.vue
+++ client/pages/reset-password/_token.vue
@@ -1,29 +1,32 @@
 <template>
   <AppLayout>
-    <form
-      @submit.prevent="submit"
-    >
-      <div>
-        <label for="newPassword">{{ $t('New password') }}</label>
-        <input
-          id="newPassword"
-          v-model="newPassword1"
-          type="password"
-        />
-      </div>
+    <main class="reset-password-token-view">
+      <h1>{{ $t('Change password') }}</h1>
+      <p>You forgot your password. Here you can change it.</p>
+      <form
+        @submit.prevent="submit"
+      >
+        <div>
+          <label for="newPassword">{{ $t('New password') }}</label>
+          <input
+            id="newPassword"
+            v-model="newPassword1"
+            type="password"
+          />
+        </div>
 
-      <div>
-        <label for="newPassword2">{{ $t('New password again') }}</label>
-        <input
-          id="newPassword2"
-          v-model="newPassword2"
-          type="password"
-        />
-      </div>
+        <div>
+          <label for="newPassword2">{{ $t('New password again') }}</label>
+          <input
+            id="newPassword2"
+            v-model="newPassword2"
+            type="password"
+          />
+        </div>
 
-
-      <button type="submit">{{ $t('Change password') }}</button>
-    </form>
+        <button type="submit">{{ $t('Change password') }}</button>
+      </form>
+    </main>
   </AppLayout>
 </template>
 
@@ -83,4 +86,51 @@ export default {
 </script>
 
 <style lang="sass">
+//- Imports
+@import './styles/components/buttons'
+
+//- Reset Password
+.reset-password-token-view
+  border-radius: 5px
+  max-width: 480px
+  margin: $baseline auto
+  padding: $baseline
+
+  background: #fff
+  border: 1px solid #ddd
+
+  //- Heading
+  > h1
+    margin-bottom: $baseline / 2
+
+    font-size: $fs-2
+    font-weight: 600
+
+  > p
+    margin-bottom: $baseline
+
+  //- Forms
+  form
+    > div
+      margin-bottom: $baseline
+
+    label
+      display: table
+
+      font-weight: 600
+
+    input
+      box-sizing: border-box
+      height: $baseline * 1.25
+      padding: 0 $baseline/4
+      width: 250px
+
+      border-radius: 5px 0 0 5px
+      border: 1px solid #ddd
+
+      font-family: $ff-sans
+      font-size: $fs-0
+
+    button
+      +button(primary, medium)
 </style>
--- client/pages/reset-password/index.vue
+++ client/pages/reset-password/index.vue
@@ -1,18 +1,33 @@
 <template>
   <AppLayout>
-    <template v-if="step === 'form'">
-      <form @submit.prevent="submit">
-        Enter your email
-        <input
-          type="email"
-          v-model="email"
-        />
-        <button type="submit">Request reset</button>
-      </form>
-    </template>
-    <template v-else>
-      Check your email.
-    </template>
+    <main class="reset-password-view">
+      <h1>{{ $t('Reset password') }}</h1>
+      <ol>
+        <li>{{ $t('Enter your email.') }}</li>
+        <li>{{ $t('We will send you email with link.') }}</li>
+        <li>{{ $t('Open the link and set your new password.') }}</li>
+      </ol>
+      <template v-if="step === 'form'">
+        <form 
+           v-if="step === 'form'"
+          @submit.prevent="submit">
+          <label for="email">{{ $t('Your email') }}</label>
+          <div>
+            <input
+              type="email"
+              id="email"
+              v-model="email"
+              placeholder="@"
+            />
+            <button type="submit">{{ $t('Request reset') }}</button>
+          </div>
+        </form>
+      </template>
+      <div class="reset-password--message" v-else>
+        <h3>{{ $t('Password reset requested') }}</h3>
+        <p>{{ $t('Check your inbox for email with the reset link.') }}</p>
+      </div>
+    </main>
   </AppLayout>
 </template>
 
@@ -32,7 +47,7 @@ export default {
 
   head() {
     return {
-      title: this.$t('Reset Password') +' – Kairly',
+      title: this.$t('Reset password') +' – Kairly',
     }
   },
 
@@ -65,4 +80,73 @@ export default {
 </script>
 
 <style lang="sass">
+//- Imports
+@import './styles/components/buttons'
+
+//- Reset Password
+.reset-password-view
+  border-radius: 5px
+  max-width: 480px
+  margin: $baseline auto
+  padding: $baseline
+
+  background: #fff
+  border: 1px solid #ddd
+
+  //- Heading
+  > h1
+    margin-bottom: $baseline / 2
+
+    font-size: $fs-2
+    font-weight: 600
+
+  //- Tips
+  ol
+    margin-bottom: $baseline
+  ol li
+    list-style: decimal inside
+
+  //- Form
+  form
+  
+    label
+      display: table
+      font-weight: 600
+
+    > div
+      display: flex
+
+
+    input
+      box-sizing: border-box
+      height: $baseline * 1.25
+      padding: 0 $baseline/4
+      width: 250px
+
+      border-radius: 5px 0 0 5px
+      border: 1px solid #ddd
+      border-right: 0
+
+      font-family: $ff-sans
+      font-size: $fs-0
+
+    button
+      +button(primary, medium)
+
+      border-radius: 0 5px 5px 0
+
+//- Message
+.reset-password--message
+  background: lighten($c-base, 30%)
+  border: 1px solid $c-base
+  padding: $baseline / 2
+
+  h3
+    margin-bottom: $baseline / 4
+
+    font-weight: 600
+  
+  p
+    font-size: $fs--1
+  
 </style>
