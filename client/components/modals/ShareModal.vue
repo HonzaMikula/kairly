6ff721e3fa824ac20e0d5de9fa83e01d9239af1c
Jan Mikula <janm@salsitasoft.com>
2020-01-12 10:00:09 +0100

    translations, mobile version, show only 5 items

--- client/components/modals/ShareModal.vue
+++ client/components/modals/ShareModal.vue
@@ -12,21 +12,38 @@
     <div>
       <h1>{{ title }}</h1>
       <ul>
-        <li v-for="post in posts" :key="post.post.id">
-          • <a :href="post.post.source">{{ post.post.content.title }}</a>
+        <li v-for="post in listOfPosts" :key="post.post.id">
+          <template v-if="post.post.content.title">
+            <nuxt-link :to="{ name: 'author-post', params: { author: post.post.author.id, post: post.post.slug }}">
+              {{ post.post.content.title }}
+            </nuxt-link>
+          </template>
+
+          <template v-else>
+            {{ post.post.author.name }}: <span v-html="post.post.content.content"></span>
+          </template>
+          
           <button @click="removeItem(post.post.id)" class="remove"></button>
         </li>
       </ul>
 
-      <p>{{ url }}</p>
+      <p class="share--url">{{ url }}</p>
+    </div>
+
+    <div class="share--show-more">
+      <button
+        v-if="(tailPostsCount > 0 && !isExpanded)"
+        @click.prevent="isExpanded = true">
+        {{ $t('Show more') }} ({{tailPostsCount}})
+      </button>
     </div>
 
     <template #footer>
-      <a href="" class="copy" @click.prevent="copyToClipboard()">Copy</a>
+      <a href="" class="copy" @click.prevent="copyToClipboard()">{{ $t('Copy') }}</a>
       <a :href="`https://www.facebook.com/sharer/sharer.php?u=${url}`" target="_blank" class="facebook">Facebook</a>
       <a :href="`https://twitter.com/intent/tweet?text=${content}&url=${url}`" target="_blank" class="twitter">Twitter</a>
       <a :href="`https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}&summary=${content}`" class="linkedin" target="_blank">LinkedIn</a>
-      <a :href="`mailto:?subject=${title}&body=${content}%0D%0A%0D%0A${url}`" class="send-by-email">Send by email</a>
+      <a :href="`mailto:?subject=${title}&body=${content}%0D%0A%0D%0A${url}`" class="send-by-email">Email</a>
     </template>
   </DialogWindow>
 </template>
@@ -34,6 +51,8 @@
 <script>
 import DialogWindow from '@/components/modals/DialogWindow'
 import ModalMixin from '@/mixins/ModalMixin'
+
+const POST_LIMIT = 5
 
 export default {
   name: 'ShareModal',
@@ -49,6 +68,7 @@ export default {
   data() {
     return {
       posts: this.issue.posts,
+      isExpanded: false
     }
   },
 
@@ -67,6 +87,17 @@ export default {
         content += '• '+ post.post.content.title +'\n'
       })
       return encodeURIComponent(content)
+    },
+
+    listOfPosts() {
+      if (!this.isExpanded)
+        return this.issue.posts.slice(0, POST_LIMIT)
+      else
+        return this.issue.posts
+    },
+
+    tailPostsCount() {
+      return Math.max(0, this.issue.posts.length - POST_LIMIT)
     }
   },
 
@@ -102,6 +133,8 @@ export default {
       padding: 0 $baseline/2
 
   main
+    position: relative
+
     padding: $baseline / 2
     overflow: auto
     -webkit-overflow-scrolling: touch
@@ -122,13 +155,19 @@ export default {
       position: relative
       
       margin-bottom: $baseline / 4
-      padding-right: $baseline
+      padding: 0 $baseline 0 $baseline/2
 
       font-size: $fs-0
 
+      &::before
+        position: absolute
+        left: 0
+
+        line-height: $baseline
+        content: ' • '
+
       a
-        display: inline-block
-        margin-left: $baseline / 4
+        display: inline
 
         color: darken($c-base, 20%)
 
@@ -156,20 +195,44 @@ export default {
           +fa-icon()
           @extend .fas
           content: fa-content($fa-var-times)
+  
+  .share--url
+    overflow: auto
+
+    font-size: $fs--1
+
+  .share--show-more
+    margin-top: $baseline / 2
+
+    button
+      +button(secondary, small)
+
+      background: #eee
+      border: 0
+
+      &:hover,
+      &:focus
+        background: #ddd
+        color: #000 
 
 
   //- Footer
   footer
     padding: 0
     display: flex
-    justify-content: space-between
-
+    justify-content: center
     a
       padding: $baseline/2 $baseline
 
       color: darken($c-base, 30%)
 
       font-size: $fs--1
+
+      @media (max-width: $mobile)
+        padding: $baseline/2
+
+        font-size: $fs--2
+        line-height: 1.42
 
       &:hover,
       &:focus
