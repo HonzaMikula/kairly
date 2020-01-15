b2bc796e79b688f7bbd1d18c184f10a8dfe17be3
Roman Krejcik <farin@farin.cz>
2020-01-15 21:20:32 +0100

    Use npm install instead of npm ci (which removes all node_models first)
    Removing node modules and reinstall takes significant time
    makingn production service unavailable

--- tasks.py
+++ tasks.py
@@ -46,7 +46,7 @@ def promote_js(ctx):
         'rm -rf /srv/kairly/client/.nuxt',
         'mv /tmp/.nuxt /srv/kairly/client/.nuxt',
         'cd /srv/app',
-        'npm ci',  # npm ci doesn't work because of Node 5.x in container
+        'npm install',
         'git reset --hard',  # reset package-lock.json back, workaround to unsupported npm ci
         "supervisorctl restart app",
     ]
