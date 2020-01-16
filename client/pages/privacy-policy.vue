11f5ff6dafacc05921818cd213b8bb7bc4974045
Jan Mikula <janm@salsitasoft.com>
2020-01-16 12:55:30 +0100

    privacy policy for czech

--- client/pages/privacy-policy.vue
+++ client/pages/privacy-policy.vue
@@ -1,52 +1,245 @@
 <template>
   <AppLayout>
     <div class="privacy-policy-view">
-      <h1>Privacy Policy for Kairly</h1>
+      <template v-if="currentLocale == 'en'">
+        <h1>Privacy Policy for Kairly</h1>
 
-      <p>At Kairly, accessible from https://kairly.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Kairly and how we use it.</p>
+        <p>At Kairly, accessible from https://kairly.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Kairly and how we use it.</p>
 
-      <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
 
-      <h2>Log Files</h2>
+        <h2>Log Files</h2>
 
-      <p>Kairly follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+        <p>Kairly follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
 
-      <h2>Cookies and Web Beacons</h2>
+        <h2>Cookies and Web Beacons</h2>
 
-      <p>Like any other website, Kairly uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+        <p>Like any other website, Kairly uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
 
-      <h2>Privacy Policies</h2>
+        <h2>Privacy Policies</h2>
 
-      <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Kairly, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Kairly, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 
-      <p>Note that Kairly has no access to or control over these cookies that are used by third-party advertisers.</p>
+        <p>Note that Kairly has no access to or control over these cookies that are used by third-party advertisers.</p>
 
-      <h2>Third Party Privacy Policies</h2>
+        <h2>Third Party Privacy Policies</h2>
 
-      <p>Kairly's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.</p>
+        <p>Kairly's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.</p>
 
-      <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
+        <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
 
-      <h2>Children's Information</h2>
+        <h2>Children's Information</h2>
 
-      <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
-      <p>Kairly does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+        <p>Kairly does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
 
-      <h2>Online Privacy Policy Only</h2>
+        <h2>Online Privacy Policy Only</h2>
 
-      <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Kairly. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+        <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Kairly. This policy is not applicable to any information collected offline or via channels other than this website.</p>
 
-      <h2>Consent</h2>
+        <h2>Consent</h2>
 
-      <p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+      </template>
 
+      <template v-else>
+        <h1>Ochrana osobních údajů</h1>
+        <p>
+        Pokud jste mým zákazníkem, odběratelem novinek nebo návštěvníkem webu,
+        svěřujete mi své osobní údaje. Já zodpovídám za jejich ochranu a
+        zabezpečení. Seznamte se, prosím, s ochranou osobních údajů, zásadami a právy,
+        které máte v souvislosti s GDPR (Nařízení o ochraně osobních údajů).
+        </p>
+        <h2>Kdo je správce?</h2>
+        <p>
+        Jsem Jan Mikula, U Pískovny 4518, 466 06, Jablonec nad Nisou a 12. 03. 1989,
+        a provozuji webové stránky https://kairly.com. Vaše
+        osobní údaje zpracovávám jako správce, tj. určuji, jak budou osobní údaje
+        zpracovávány a za jakým účelem, po jak dlouhou dobu a vybírám případné další
+        zpracovatele, kteří mi se zpracováním budou pomáhat.
+        </p>
+        <h2>Kontaktní údaje</h2>
+        <p>
+        Pokud se na nás/mě budete chtít v průběhu zpracování obrátit, můžete nás/mě kontaktovat
+        na tel. čísle +420 739 222 781 nebo na e-mail: <a href="mailto:jan.mikula@kairly.com">jan.mikula@kairly.com</a>.
+        </p>
+        <h3>Prohlašujeme</h3>
+
+        <p>
+        Prohlašuji, že jako správce vašich osobních údajů splňuji veškeré zákonné
+        povinnosti vyžadované platnou legislativou, zejména zákonem o ochraně osobních údajů a
+        GDPR, a tedy že:
+        budu zpracovávat vaše osobní údaje jen na základě platného právního důvodu, a to
+        především oprávněného zájmu, plnění smlouvy, zákonné povinnosti či uděleného souhlasu,
+        plním dle článku 13 GDPR informační povinnost ještě před zahájením zpracování
+        osobních údajů,
+        umožním a budu vás podporovat v uplatňování a plnění vašich
+        práv podle zákona o ochraně osobních údajů a GDPR
+        Rozsah osobních údajů a účely zpracování
+        Zpracovávám osobní údaje, které mi svěříte sami, a to z následujících
+        důvodů (pro naplnění těchto účelů):
+        </p>
+
+        <ul>
+          <li>
+          marketing - zasílání newsletterů
+          Vaše osobní údaje (e-mail a jméno). Jste-li naším zákazníkem, děláme tak z oprávněného zájmu, neboť důvodně
+          předpokládáme, že vás naše novinky zajímají, a to po dobu 5 let od registrace.
+          </li>
+        </ul>
+        <p>
+        Pokud nejste naším zákazníkem, zasíláme vám newslettery jen na základě vašeho
+        souhlasu, po dobu 5 let od udělení. V obou případech můžete tento souhlas odvolat
+        použitím odhlašovacího odkazu v každém zaslaném e-mailu.
+        </p>
+
+        <h2>Cookies</h2>
+        <p>
+        Při procházení mých webových stránek zaznamenáváme vaši IP adresu, jak dlouho
+        se na stránce zdržíte a ze které stránky přicházíte. Používání cookies pro měření
+        návštěvnosti webu a přizpůsobení zobrazení webových stránek vnímáme jako svůj
+        oprávněný zájem správce, neboť věříme, že díky tomu vám můžeme nabídnout ještě lepší
+        služby.
+        </p>
+        <p>
+        Cookies pro cílení reklamy budou zpracovávány jen na základě vašeho souhlasu.
+        Naše/moje webové stránky lze procházet také v režimu, který neumožňuje sbírání osobních
+        údajů. Používání cookies můžete na svém počítači zakázat.
+        </p>
+
+        <h2>Zabezpečení a ochrana osobních údajů</h2>
+        
+        <p>
+        Chráním osobní údaje v maximální možné míře pomocí moderních technologií, které
+        odpovídají stupni technického rozvoje. Chráním je jako kdyby byly naše vlastní.
+        Přijal jsme a udržujeme veškerá možná (aktuálně známá) technická a
+        organizační opatření, která zamezují zneužití, poškození nebo zničení vašich osobních
+        údajů.
+        </p>
+
+        <h2>Předání osobních údajů třetím osobám</h2>
+        
+        <p>
+        K vašim osobním údajům mají přístup moji zaměstnanci a spolupracovníci.
+        Pro zajištění konkrétních zpracovatelských operací, které nedokáži zajistit
+        vlastními silami, využívám služeb a aplikací zpracovatelů, kteří umí data ochránit
+        ještě lépe než já a na dané zpracování se specializují.
+        </p>
+
+        <p>
+        Jsou to poskytovatelé následujících platforem:
+        </p>
+        <ul>
+          <li>MailChimp</li>
+        </ul>
+        <p>
+        Je možné, že se v budoucnu rozhodnu využít další aplikace či zpracovatele,
+        pro usnadnění a zkvalitnění zpracování. Slibuji vám však, že v takovém případě
+        při výběru, budu na zpracovatele klást minimálně stejné nároky na zabezpečení a kvalitu
+        zpracování jako na sebe.
+        </p>
+        <h2>Předávání dat mimo Evropskou unii</h2>
+        <p>
+        Data zpracováváme výhradně v Evropské unii nebo v zemích, které zajišťují
+        odpovídající úroveň ochrany na základě rozhodnutí Evropské komise.
+        
+        <h2>Vaše práva v souvislosti s ochranou osobních údajů</h2>
+
+        <p>
+        V souvislosti s ochranou osobních údajů máte řadu práv. Pokud budete chtít některého z
+        těchto práv využít, prosím, kontaktujte mě prostřednictvím e-mailu: <a href="mailto:jan.mikula@kairly.com">jan.mikula@kairly.com</a>.
+        Máte právo na informace, které je plněno již touto informační stránkou se zásadami
+        zpracování osobních údajů.
+        </p> 
+
+        <p>
+        Díky právu na přístup mě můžete kdykoli vyzvat a já vám doložím ve lhůtě 14 dní, jaké
+        vaše osobní údaje zpracovávám a proč.
+        </p>
+
+        <p>
+        Pokud se u vás něco změní nebo shledáte své osobní údaje neaktuální nebo neúplné, máte
+        právo na doplnění a změnu osobních údajů.
+        </p>
+        <p>
+        Právo na omezení zpracování můžete využít, pokud se domníváte, že
+        zpracovávám vaše nepřesné údaje, domníváte se, že provádím
+        zpracování nezákonně, ale nechcete všechny údaje smazat nebo pokud jste vznesl námitku
+        proti zpracování.
+        </p>
+        <p>
+        Omezit můžete rozsah osobních údajů nebo účelů zpracování. (Např. odhlášením z
+        newsletteru omezujete účel zpracování pro zasílání obchodních sdělení.)
+        </p>
+        
+        <h3>Právo na přenositelnost</h3>
+        <p>
+        Pokud byste chtěli své osobní údaje vzít a přenést k někomu jinému, budeme postupovat
+        stejně jako při využití práva na přístup - jen s tím rozdílem, že vám informace pošlu
+        ve strojově čitelné podobě. Tady potřebuji alespoň 14 dní.
+        </p>
+
+        <h3>Právo na výmaz (být zapomenut)</h3>
+        <p>
+        Vaším dalším právem je právo na výmaz (být zapomenut). Nechci na vás
+        zapomenout, ale pokud si to budete přát, máte na to právo. V takovém případě
+        vymaži veškeré vaše osobní údaje od sebe ze svého systému i ze systému
+        všech dílčích zpracovatelů a záloh. Na zajištění práva na výmaz potřebuji 14
+        dní.
+        </p>
+
+        <p>
+        V některých případech jsem vázán zákonnou povinností, a např. musím
+        evidovat vystavené daňové doklady po lhůtu stanovenou zákonem. V tomto případě tedy
+        smaži všechny takové osobní údaje, které nejsou vázány jiným zákonem. O
+        dokončení výmazu vás budu informovat na e-mail. 
+        </p>
+
+        <h3>Stížnost u Úřadu na ochranu osobních údajů</h3>
+
+        <p>
+        Pokud máte pocit, že s vašimi údaji nezacházím v souladu se zákonem, máte
+        právo se se svou stížností kdykoli obrátit na Úřad pro ochranu osobních údajů. Budu rád
+        pokud nejprve budete o tomto podezření informovat mě,
+        abych s tím mohl něco udělat a případné pochybení napravit.
+        </p>
+
+        <h3>Odhlášení ze zasílání newsletterů a obchodních sdělení</h3>
+
+        <p>
+        E-maily s inspirací, články či produkty a službami vám zasílám/e jste-li můj zákazník na
+        základě mého oprávněného zájmu.
+        </p>
+
+        <p>
+        Pokud zákazníkem ještě nejste, posílám vám je jen na základě vašeho souhlasu. V obou
+        případech můžete ukončit odběr mých e-mailů stisknutím odhlašovacího odkazu v
+        každém zaslaném e-mailu.
+        </p>
+
+        <h3>Mlčenlivost</h3>
+
+        <p>
+        Dovolujeme si Vás ujistit, že naši zaměstnanci i spolupracovníci, kteří budou
+        zpracovávat Vaše osobní údaje, jsou povinni zachovávat mlčenlivost o osobních
+        údajích a o bezpečnostních opatřeních, jejichž zveřejnění by ohrozilo zabezpečení
+        Vašich osobních údajů. Tato mlčenlivost přitom trvá i po skončení závazkových
+        vztahů s námi. Bez vašeho souhlasu nebudou vaše osobní údaje vydány žádné jiné
+        třetí straně.
+        </p>
+
+        <p>
+        Tyto zásady zpracování osobních údajů platí od 1.1.2020.
+        </p>
+      </template>
       <FooterLinks />
     </div>
   </AppLayout>
 </template>
 
 <script>
+import { mapState } from "vuex"
 import AppLayout from '@/components/layout/AppLayout'
 import FooterLinks from '@/components/microsite/FooterLinks'
 
@@ -58,6 +251,12 @@ export default {
   components: {
     AppLayout,
     FooterLinks
+  },
+
+  computed: {
+    ...mapState({
+      currentLocale: state => state.locale || 'en'
+    })
   },
 
   head() {
@@ -106,8 +305,24 @@ export default {
     font-size: $fs-1
     font-weight: 600
 
+  h3
+    margin-bottom: $baseline
+
+    font-family: $ff-serif
+    font-weight: 600
+
   p
     margin-bottom: $baseline
 
     font-family: $ff-serif
+
+  ul
+    margin-bottom: $baseline
+    
+    font-family: $ff-serif
+
+  li
+    margin-bottom: $baseline / 2
+    list-style: disc
+
 </style>
