c2dc1be8b3bd4ae09937ea271d1c37b764f0a49d
Roman Krejcik <farin@farin.cz>
2020-01-31 21:56:20 +0100

    tweer video attachments improved

--- client/components/posts/PostBase.vue
+++ client/components/posts/PostBase.vue
@@ -23,6 +23,7 @@
         </h3>
 
         <AuthorPopup
+         v-if="post.author.kind != 'external'"
           :target="`post-author-${post.id}`"
           :author="post.author"
         />
--- client/components/posts/TweetAttachmentVideo.vue
+++ client/components/posts/TweetAttachmentVideo.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="tweet-attachment-video-view">
-    <video :poster="item.src" :width="item.sizes.small.w" controls>
-      <source
-        :src="item.video_info.variants[0].url"
-        :type="item.video_info.variants[0].content_type" />
+    <video
+      :poster="item.src"
+      :preload="none"
+      :width="dim[0]"
+      :height="dim[1]"
+      :duration="item.video_info.duration_millis / 1000"
+      controls
+      playsinline
+    >
+      <!--source
+        v-for="(source, idx) in item.video_info.variants"
+        :key="idx"
+        :src="source.url"
+        :type="source.content_type"
+      /-->
     </video>
   </div>
 </template>
 
 <script>
+const MAX_SIZE = 576
+
 export default {
   name: 'tweet-attachment-video',
 
   props: ["item"],
+
+  computed: {
+    dim() {
+      const ar = this.item.video_info.aspect_ratio
+      const dim1 = [MAX_SIZE, parseInt(MAX_SIZE / ar[0] * ar[1])]
+      const dim2 = [parseInt(MAX_SIZE / ar[1] * ar[0]), MAX_SIZE]
+      return dim1[1] > MAX_SIZE ? dim2 : dim1
+    }
+  }
 }
 </script>
 
@@ -23,6 +45,6 @@ export default {
 
   video
     display: block
-    width: 100%
+    max-width: 100%
 
 </style>
