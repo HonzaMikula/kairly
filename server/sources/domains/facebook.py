cfc12e2d7657f6d32b157df50235e54866c7282f
Roman Krejcik <farin@farin.cz>
2020-04-12 12:10:56 +0200

    Improve import of facebook notes

--- server/sources/domains/facebook.py
+++ server/sources/domains/facebook.py
@@ -6,13 +6,38 @@ import requests
 
 from sources.parser import ArticleParser, split_article_to_perex_and_content
 
-RE_FACEBOOK_ALERNATE = re.compile(r'https://(m|www).facebook.com/([^/]+)/.*')
+RE_FACEBOOK_ALERNATE = re.compile(r'https://(m|www).facebook.com/([^/]+)/?.*')
 RE_FACEBOOK_POST = re.compile(r'https://(m|www).facebook.com/([^/]+)/posts/(\d+)/?(\?.*)?')
 RE_FACEBOOK_GROUP_POST = re.compile(r'https://(m|www).facebook.com/groups/([^/]+)/permalink/(\d+)/?(\?.*)?')
 RE_FACEBOOK_PHOTO = re.compile(r'https://www.facebook.com/photo.php\?fbid=(\d+).*')
 RE_FACEBOOK_PHOTOS_PHOTO = re.compile(r'https://(m|www).facebook.com/([^/]+)/photos/([^/]+)/(\d+).*')
+RE_FACEBOOK_NOTE = re.compile(r'https://(m|www).facebook.com/notes/([^/]+)/([^/]+)/(\d+).*')
 
 RE_EMOJI_STYLE = re.compile(r'height: 16px; width: 16px; font-size: 16px; background-image: url\("([^"]+)"\)')
+
+
+def download_icon(url):
+    resp = requests.get(url)
+    if resp.ok:
+        content_type = resp.headers['Content-Type']
+        content = base64.b64encode(resp.content).decode()
+        return f"data:{content_type};base64,{content}"
+
+
+def get_background_image(el):
+    url = el.attrib['style'].split(':', maxsplit=1)[1].strip()[5:-2].replace(r'\/', '/')
+    return download_icon(url)
+
+
+def parse_content(el):
+    parser = ArticleParser('*')
+    fragments = parser.parse(el)
+    fragments = parser.normalize(fragments)
+    perex, content = split_article_to_perex_and_content(fragments, [340, 660])
+    return {
+        'perex': perex,
+        'content': content
+    }
 
 
 def extend_facebook_link(source, guid, fb_user, keep_image, post_args, htmltree):
@@ -20,10 +45,7 @@ def extend_facebook_link(source, guid, fb_user, keep_image, post_args, htmltree)
     try:
         el = htmltree.cssselect('.userContentWrapper .clearfix img')[0]
         author_name = el.attrib['aria-label']
-        icon_resp = requests.get(el.attrib['src'])
-        content_type = icon_resp.headers['Content-Type']
-        content = base64.b64encode(icon_resp.content).decode()
-        icon = f"data:{content_type};base64,{content}"
+        icon = download_icon(el.attrib['src'])
 
         if fb_user is None:
             for el in htmltree.cssselect('link[rel=alternate]'):
@@ -64,16 +86,45 @@ def extend_facebook_link(source, guid, fb_user, keep_image, post_args, htmltree)
                     for child in list(el):
                         el.remove(child)
 
-            parser = ArticleParser('*')
-            fragments = parser.parse(message_el)
-            fragments = parser.normalize(fragments)
-            perex, content = split_article_to_perex_and_content(fragments, [300, 600])
-            post_args['perex'] = perex
-            post_args['content'] = content
+            post_args.update(parse_content(message_el))
 
         post_args['attachments']['author'] = author
         post_args['title'] = None
         post_args['guid'] = guid
+    except IndexError as e:
+        print(e)
+    return post_args
+
+
+def extend_facebook_note(source, guid, post_args, htmltree):
+    post_args['source'] = source
+    try:
+        el_icon = htmltree.cssselect('.fb_content .clearfix .lfloat div[style]')[0]
+        el_userbox = el_icon.getparent().getparent()
+        el_header = el_userbox.getparent()
+        el_content = el_header.getparent().getchildren()[1]
+
+        post_args['guid'] = guid
+        post_args['title'] = el_header.getchildren()[1].text
+
+        icon = get_background_image(el_icon)
+        fb_user = None
+        el_user = el_userbox.getchildren()[1].cssselect('a')[0]
+        href = el_user.attrib['href']
+        m = RE_FACEBOOK_ALERNATE.fullmatch(href)
+        if m:
+            fb_user = m.group(2)
+
+        author = {
+            'name': el_user.text,
+            'image': icon
+        }
+        if fb_user:
+            author['id'] = fb_user
+            author['profile_url'] = f"https://www.facebook.com/{fb_user}"
+
+        post_args['attachments']['author'] = author
+        post_args.update(parse_content(el_content))
     except IndexError as e:
         print(e)
     return post_args
@@ -85,21 +136,21 @@ class FacebookImporter:
     def match(self, url):
         m = RE_FACEBOOK_POST.fullmatch(url)
         if m:
-            fb_user = m.group(2)
-            fb_post_id = m.group(3)
-            source = f"https://www.facebook.com/{fb_user}/posts/{fb_post_id}"
-            guid = f'fb|{fb_user}_{fb_post_id}'
+            user = m.group(2)
+            post_id = m.group(3)
+            source = f"https://www.facebook.com/{user}/posts/{post_id}"
+            guid = f'fb|{user}_{post_id}'
 
             url = source + '?_fb_noscript=1'
-            extend_callback = partial(extend_facebook_link, source, guid, fb_user, False)
+            extend_callback = partial(extend_facebook_link, source, guid, user, False)
             return (url, guid, extend_callback)
 
         m = RE_FACEBOOK_GROUP_POST.fullmatch(url)
         if m:
-            fb_group_id = m.group(2)
-            fb_post_id = m.group(3)
-            source = f"https://www.facebook.com/groups/{fb_group_id}/permalink/{fb_post_id}"
-            guid = f'fb|{fb_group_id}_{fb_post_id}'
+            group_id = m.group(2)
+            post_id = m.group(3)
+            source = f"https://www.facebook.com/groups/{group_id}/permalink/{post_id}"
+            guid = f'fb|{group_id}_{post_id}'
 
             url = source + '?_fb_noscript=1'
             extend_callback = partial(extend_facebook_link, source, guid, None, False)
@@ -107,9 +158,9 @@ class FacebookImporter:
 
         m = RE_FACEBOOK_PHOTO.fullmatch(url)
         if m:
-            fb_photo_id = m.group(1)
-            source = f"https://www.facebook.com/photo.php?fbid={fb_photo_id}"
-            guid = f'fb|photo:{fb_photo_id}'
+            photo_id = m.group(1)
+            source = f"https://www.facebook.com/photo.php?fbid={photo_id}"
+            guid = f'fb|photo:{photo_id}'
 
             url = source + '&_fb_noscript=1'
             extend_callback = partial(extend_facebook_link, source, guid, None, True)
@@ -117,12 +168,24 @@ class FacebookImporter:
 
         m = RE_FACEBOOK_PHOTOS_PHOTO.fullmatch(url)
         if m:
-            fb_user = m.group(2)
-            fb_album_id = m.group(3)
-            fb_post_id = m.group(4)
-            source = f"https://www.facebook.com/{fb_user}/photos/{fb_album_id}/{fb_post_id}"
-            guid = f'fb|photo:{fb_album_id}/{fb_post_id}'
+            user = m.group(2)
+            album_id = m.group(3)
+            post_id = m.group(4)
+            source = f"https://www.facebook.com/{user}/photos/{album_id}/{post_id}"
+            guid = f'fb|photo:{album_id}/{post_id}'
 
             url = source + '?_fb_noscript=1'
-            extend_callback = partial(extend_facebook_link, source, guid, fb_user, True)
+            extend_callback = partial(extend_facebook_link, source, guid, user, True)
+            return (url, guid, extend_callback)
+
+        m = RE_FACEBOOK_NOTE.fullmatch(url)
+        if m:
+            user_title = m.group(2)
+            note_slug = m.group(3)
+            note_id = m.group(4)
+            source = f"https://www.facebook.com/notes/{user_title}/{note_slug}/{note_id}"
+            guid = f'fb|note:{note_slug}/{note_id}'
+
+            url = source + '?_fb_noscript=1'
+            extend_callback = partial(extend_facebook_note, source, guid)
             return (url, guid, extend_callback)
