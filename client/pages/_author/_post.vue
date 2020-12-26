69b301fa76c9c022aa32b9785dbd1370c2f44eeb
Jan Mikula <jann@salsitasoft.com>
2020-12-26 15:57:47 +0100

    Fix footer in short articles
    closes #582

--- client/pages/_author/_post.vue
+++ client/pages/_author/_post.vue
@@ -34,7 +34,7 @@
           </div>
         </no-ssr>
 
-        <div v-if="post.content.protected && post.source && (!loggedIn || !post.content.content)">
+        <div v-if="post.content.protected && post.source && !loggedIn">
           <footer class="post-detail--footer">
             <a :href="post.source" class="read-full-article">{{ $t('Read full article') }}</a>
           </footer>
